77148ac623b98c8a865b5727bae25d6c4e75998a
msempere <odoncopon@gmail.com>
2014-12-30 20:28:30 +0000

    Included setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='redis_containers',
+            version='0.1',
+            description='Container data structures for Redis',
+            url='https://github.com/msempere/redis_containers',
+            author='msempere',
+            author_email='msempere@gmx.com',
+            license='MIT',
+            packages=['redis_containers'],
+            test_suite="tests",
+            zip_safe=False)
